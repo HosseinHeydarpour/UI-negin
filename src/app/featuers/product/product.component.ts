cb83bc0c7234ed9dbc44cb671954014710d36330
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-01 01:03:53 +0330

    feat: add product card

--- src/app/featuers/product/product.component.ts
+++ src/app/featuers/product/product.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { ProductCardComponent } from '../../shared/product-card/product-card.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ProductCardComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
